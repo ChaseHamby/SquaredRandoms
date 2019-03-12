263d63643019877c94ef6af314f2145726b9d5c0
unknown <c.chase.hamby@gmail.com>
2019-03-12 13:17:38 -0500

    working

---
+++ SquaredRandoms/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SquaredRandoms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            var randomList = new List<int>();
+            var squaredList = new List<int>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                randomList.Add(rnd.Next(50));
+            }
+
+            foreach (var number in randomList)
+            {
+                var squared = number * number;
+                if (squared % 2 != 1)
+                {
+                    squaredList.Add(squared);
+                }
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine($"Odd numbers removed - here is the final list: ");
+
+            foreach (var result in squaredList)
+            {
+                Console.WriteLine(result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
